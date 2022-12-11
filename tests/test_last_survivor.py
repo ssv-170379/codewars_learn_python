93deae55cb58d10662bd1d7f05090f5e4fa1c558
ssv-170379 <ssv_170379@ukr.net>
2022-12-11 23:41:07 +0200

    Add solution: Last Survivor

---
+++ tests/test_last_survivor.py
@@ -0,0 +1,17 @@
+import codewars_test as test  # https://github.com/codewars/python-test-framework
+
+import os, sys
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))  # add parent directory to system path (to import test objects from ../solution/)
+
+from solution.kyu_7.last_survivor import last_survivor
+
+
+@test.describe("Example")
+def test_group():
+    @test.it("test case")
+    def test_case():
+        test.assert_equals(last_survivor('abc', [1, 1]), 'a')
+        test.assert_equals(last_survivor('kbc', [0, 1]), 'b')
+        test.assert_equals(last_survivor('zbk', [2, 1]), 'z')
+        test.assert_equals(last_survivor('c', []), 'c')
+        test.assert_equals(last_survivor('foiflxtpicahhkqjswjuyhmypkrdbwnmwbrrvdycqespfvdviucjoyvskltqaqirtjqulprjjoaiagobpftywabqjdmiofpsr', [8, 59, 52, 93, 21, 40, 88, 85, 59, 10, 82, 18, 74, 59, 51, 47, 75, 49, 23, 56, 1, 33, 39, 33, 34, 44, 25, 0, 51, 25, 36, 32, 57, 10, 57, 12, 51, 55, 24, 55, 31, 49, 6, 15, 10, 48, 27, 29, 38, 30, 35, 42, 23, 32, 9, 39, 39, 36, 8, 29, 2, 33, 14, 3, 13, 25, 9, 25, 18, 10, 1, 2, 20, 8, 2, 11, 5, 7, 0, 10, 10, 8, 12, 3, 5, 1, 7, 7, 5, 1, 4, 0, 4, 0, 0, 1]), 'd')
